887761f366b74688eeaff191c175b9f9415074fe
Szymon Mosiolek <szymon.mosiolek@profound.works>
2018-06-03 13:07:42 +0200

    Toggle global function added, replace Accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,17 +1,31 @@
-/**
+/* Acoordion
+ *
  * https://www.w3schools.com/howto/howto_js_accordion.asp
  */
-var acc = document.getElementsByClassName("accordion__title");
+var acc = document.getElementsByClassName("js--toggle");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("is-open");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("is-open");
+
+		var panel = this.nextElementSibling;
+
+		// Toggle vertical 
+		if (panel.style.maxHeight && this.dataset.toggle == 'vertical') {
+			panel.style.maxHeight = null;
+		} 
+		else if (!panel.style.maxHeight && this.dataset.toggle == 'vertical') {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+
+		// Toggle horizontal
+		if (panel.style.maxWidth && this.dataset.toggle == 'horizontal') { 
+			panel.style.maxWidth = null;
+		} 
+		else if (!panel.style.maxWidth && this.dataset.toggle == 'horizontal') {
+			panel.style.maxWidth = panel.scrollWidth + "px";
+		} 
+	});
 }
